043c93c9c052916127e4ae1f6b019d53d1e1d1ab
WWoo <info@sonerodabas.com>
2023-02-27 15:46:08 +0300

    Create connect.sh

---
+++ connect.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+VPNFILE="./vpnsettings.conf"
+VPNON=""
+while [ -z "$VPNON" ]
+do
+    echo
+	echo -e "Credentials"
+	echo -e "================================="
+	echo "Configurant"
+	echo $HOST > $VPNFILE
+	echo $PORT >> $VPNFILE
+	echo $USERNAME >> $VPNFILE
+	echo $PASSWORD >> $VPNFILE
+	echo $TRUSTED >> $VPNFILE
+    echo
+	echo "Connecting to vpn"
+	VPNON=""
+    sudo openfortivpn -c $VPNFILE > openfortivpn.log 2>&1 &
+   COUNT=1
+    while [ -z "$VPNON" ]
+	do
+	    echo -ne "\e[33mCONNECTING TO VPN ... $COUNT\033[0K\r"
+		VPNON='ip a | grep ppp0'
+		if [ ! -z "$VPNON" ]
+		then
+            echo
+			echo -e "\e[32mVPN. Ok "
+			break
+		fi
+		if [ $COUNT -eq 10 ];
+		then
+			echo ""
+			echo ""
+			echo -e "\e[31mERROR: VPN Connection Error. "
+			break
+		fi
+		let COUNT=$COUNT+1
+		sleep 1
+	done
+done
